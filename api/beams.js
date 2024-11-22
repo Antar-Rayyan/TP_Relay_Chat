ea0c0649694742841acd65bfbe6e05f11260bae1
Mohamed Rayyan ANTAR <rayyan.antar02@gmail.com>
2024-11-22 11:29:10 +0100

    debug

--- api/beams.js
+++ api/beams.js
@@ -15,8 +15,8 @@ export default async (req, res) => {
 
     console.log("Using push instance : " + process.env.PUSHER_INSTANCE_ID);
     const beamsClient = new PushNotifications({
-        instanceId: process.env.PUSHER_INSTANCE_ID,
-        secretKey: process.env.PUSHER_SECRET_KEY,
+        instanceId: "7e69ae50-bb55-4360-9819-4c8bfd05a7b2",
+        secretKey: "3CDA930BEE66856757EE11118C47C11F868E9F03F95E581654C42614E10B356B",
     });
 
     const beamsToken = beamsClient.generateToken(user.externalId);
